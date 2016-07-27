c6a4e3900f795a7fa88ad08e0bd8349c1b43f237
HanQi <1464467970@qq.com>
2016-07-28 01:34:58 +0800

    项目搭建

---
+++ PersonalCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonalCenter
+//
+//  Created by HanQi on 16/7/28.
+//  Copyright © 2016年 HanQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
